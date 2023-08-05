f5f487aa84ed78b75d6e7e722def38b755bdb4ca
orokusaki31 <bansan556@gmail.com>
2023-08-04 19:16:47 -0500

    040723

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 
-console.log('nodejs - express');
+// importar el paquete de express para el wedserver
+const express = require('express');
+// inicializar la aplicacion con el archivo app.js
+const app = express();
+// para activar el puerto 
+const puerto = 7475;
+
+// para mostrar los archivos de la carperta public se utiliza metodo use
+
+app.use(express.static('public'));
+
+
+
