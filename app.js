4652386180f11b7a0c5911aede8d001c580318ae
orokusaki31 <bansan556@gmail.com>
2023-08-04 20:37:05 -0500

    0807232

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 // importar el paquete de express para el wedserver
 const express = require('express');
+
 // inicializar la aplicacion con el archivo app.js
 const app = express();
 // para activar el puerto 
@@ -10,5 +11,31 @@ const puerto = 7475;
 
 app.use(express.static('public'));
 
+// crear la srutas de las paginas wed, se utiliza el metodo get
 
+app.get('/left-sidebar', (req, res) => {
+    //  res para dar una respuesta de peticion al usuario 
+    res.sendFile(__dirname + '/public/letf-sidebar.html');
 
+})
+app.get('/no-sidebar', (req, res) => {
+    //  res para dar una respuesta de peticion al usuario 
+    res.sendFile(__dirname + '/public/letf-sidebar.html');
+
+})
+app.get('/right-sidebar', (req, res) => {
+    //  res para dar una respuesta de peticion al usuario 
+    res.sendFile(__dirname + '/public/letf-sidebar.html');
+
+})
+app.get('*', (req, res) => {
+    //  el comodin o * 
+    res.sendFile(__dirname + '/public/404.html');
+
+})
+
+//activar el puerto
+app.listen(puerto,()=>{
+    // imprimir en al cosola, si se activo con exito el wedserver
+console.log(`el wedserver esta activo en el puerto ${puerto}`)
+})
